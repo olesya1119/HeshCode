13773cdbad9b92df59cb380433d9458fd24c2eab
olesya1119 <bulchuk.olesya@gmail.com>
2023-12-13 22:31:11 +0700

    Добавьте файлы проекта.

---
+++ HeshCode/FilesWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HeshCode
+{
+    /// <summary>
+    /// Логика взаимодействия для FilesWindow.xaml
+    /// </summary>
+    public partial class FilesWindow : Window
+    {
+        public FilesWindow()
+        {
+            InitializeComponent();
+            string[][] accetsMatrix;
+            using (FileStream fstream = new FileStream("../../files/loginDetails.txt", FileMode.OpenOrCreate))
+            {
+                byte[] buffer = new byte[fstream.Length];
+                // считываем данные
+                fstream.Read(buffer, 0, buffer.Length);
+                // декодируем байты в строку
+                string textFromFile = Encoding.Default.GetString(buffer);
+
+                string[] logins = textFromFile.Split(new char[] {'\n' });
+            }
+        }
+    }
+}
---
+++ HeshCode/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HeshCode
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] profiles;
+            using (FileStream fstream = new FileStream("../../files/loginDetails.txt", FileMode.OpenOrCreate))
+            {
+                byte[] buffer = new byte[fstream.Length];
+                // считываем данные
+                fstream.Read(buffer, 0, buffer.Length);
+                // декодируем байты в строку
+                string textFromFile = Encoding.Default.GetString(buffer);
+                profiles = textFromFile.Split(new char[] {' ', '\n'});         
+            }
+            for (int i = 0; i < profiles.Length; i+=2)
+            {
+                if (profiles[i] == textbox_login.Text && profiles[i+1] == CreateSHA512(passwordbox_password.Password))
+                {
+                    //itsUser = true;
+                }
+            }
+
+            error_message.Text = "Пароль или логин введины неверно. Попробуйте заново!";
+            
+        }
+
+
+
+        public static string CreateSHA512(string source)
+        {
+            using (SHA512 sha512Hash = SHA512.Create())
+            {
+                byte[] sourceBytes = Encoding.UTF8.GetBytes(source);
+                byte[] hashBytes = sha512Hash.ComputeHash(sourceBytes);
+                string hash = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+
+                return hash;
+            }
+        }
+    }
+}
